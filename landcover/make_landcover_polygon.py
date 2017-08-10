b18d359b342a41f7e787ccc5df7d255e6bbc411b
tattii <yuta.tatti@gmail.com>
2017-08-10 19:40:57 +0900

    make_landcover_polygon

---
+++ landcover/make_landcover_polygon.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+import os, glob
+from joblib import Parallel, delayed
+
+import vectorize
+
+
+def polygonize(src, src_dir, dst_dir):
+    dst = dst_dir + '/' + src[:-3] + 'json'
+    print dst
+    if src[-3:] != 'tif' or os.path.exists(dst): return
+
+    vectorize.vectorizeRaster(src_dir + '/' + src, dst)
+
+
+def process(src_dir, dst_dir):
+    files = os.listdir(src_dir)
+    print len(files), 'files'
+
+    if not os.path.exists(dst_dir): os.makedirs(dst_dir)
+    r = Parallel(n_jobs=2, verbose=10)(
+        delayed(polygonize)(file, src_dir, dst_dir) for file in files)
+
+def main(src_dir, dst_dir):
+    for z in range(10, 11):
+        print 'z', z
+        src = src_dir + '/z' + str(z) 
+        dst = dst_dir + '/z' + str(z) 
+
+        process(src, dst)
+
+
+if __name__ == '__main__':
+    main('tdata/layer', 'tdata/polygon')
+
