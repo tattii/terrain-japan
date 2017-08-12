f436284b78186db31cc84adf8ad61e70cc04bc2e
tattii <yuta.tatti@gmail.com>
2017-08-12 19:27:01 +0900

    merged scale down

--- hillshade/make_hillshade_layers.py
+++ hillshade/make_hillshade_layers.py
@@ -1,9 +1,10 @@
 # -*- coding: utf-8 -*-
 import os, sys, subprocess, glob
 import math, re
-import commands
+import commands, shutil
 from osgeo import gdal
 
+gdalcache = '--config GDAL_CACHEMAX 1024'
 
 def warpraster(file, dst):
     command = 'gdalwarp -t_srs EPSG:3785 -r bilinear %s %s' % (file, dst)
@@ -92,6 +93,30 @@ def hillshade_tile(base, src, dst_merge, dst_hillshade):
 
     os.remove(dst)
 
+
+def scaleraster_res(file, dst, res, resampling='bilinear'):
+    command = 'gdalwarp %s -t_srs EPSG:3785 -r %s -tr %d %d %s %s' % (gdalcache, resampling, res, res, file, dst)
+    print command
+    subprocess.call(command, shell=True)
+
+def retile_auto(file, dstd):
+    x, y = getsize(file)
+
+    if x > 1500 or y > 1500:
+        tx, ty = x, y
+        while tx > 1500 or ty > 1500:
+            tx = tx / 2.
+            ty = ty / 2.
+        tx = math.ceil(tx)
+        ty = math.ceil(ty)
+        command = 'gdal_retile.py %s -ps %d %d -targetDir %s %s' % (gdalcache, tx, ty, dstd, file)
+        print command
+        subprocess.call(command, shell=True)
+
+    else:
+        shutil.copy(file, dstd + '/' + os.path.basename(file))
+
+
 def tile_coord(dst_tile, lat, lon, x, y, split, dx, dy):
     x += dx
     if x <= 0:
@@ -153,8 +178,7 @@ def scale_tiles(files, dst_dir, z, scale, split):
         dst = dst_base + os.path.basename(file)[:-4] + '.tif'
         scalefile(file, dst, scale)
 
-        if split != 1:
-            retile(dst, dst_tile, split)
+        retile(dst, dst_tile, split)
 
     hillshade_tiles(dst_tile, dst_dir, z, split)
 
@@ -172,15 +196,44 @@ def base_tiles(files, dst_dir, z):
     hillshade_tiles(dst_tile, dst_dir, z, 4)
 
 
+def vrt_tiles(vrt, dst_dir, z):
+    dst_base = dstd(dst_dir, 'base', z)
+    dst_vrt = dstd(dst_dir, 'vrt', z)
+    dst_hillshade = dstd(dst_dir, 'hillshade', z)
+        
+    scale = 1.5 ** (10 - z)
+    res = 120 * scale  # resolution (m)
+
+    scaled = dst_base + os.path.basename(vrt)[:-4] + '.tif'
+    scaleraster_res(vrt, scaled, res)
+    hillshaded = dst_vrt + 'all-hillshade.tif'
+    hillshade(scaled, hillshaded)
+
+    retile_auto(hillshaded, dst_hillshade)
+
+
 def main(src_dir, dst_dir):
     files = glob.glob(src_dir + '/*.hgt')
     print len(files), 'files'
 
     #base_tiles(files, dst_dir, 12)
 
-    scale_tiles(files, dst_dir, 13, 2, 8)
+    # scale up
+    #scale_tiles(files, dst_dir, 13, 2, 8)
     #scale_tiles(files, dst_dir, 14, 4, 16)
 
+    # scale down
+    #scale_tiles(files, dst_dir, 11, 1./2, 2)
+    #scale_tiles(files, dst_dir, 10, 1./4, 1)
+
+    # vrt z10 tiles
+    vrt = dst_dir + '/z10-tiles.vrt'
+    vrt_files = glob.glob(dst_dir + '/tile/z10/*.tif')
+    buildvrt(vrt_files, vrt)
+
+    # merged scale down 3-9
+    for z in range(3, 10):
+        vrt_tiles(vrt, dst_dir, z)
 
 if __name__ == '__main__':
     main('srtm0', 'sdata')
