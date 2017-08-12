25c781b6f2e0ecfd09781d20340305856782d8cc
tattii <yuta.tatti@gmail.com>
2017-08-12 17:50:42 +0000

    copy from landcover

--- hillshade/make_vector_tiles.py
+++ hillshade/make_vector_tiles.py
@@ -3,13 +3,23 @@ import os
 import subprocess
 
 def vtile(src_dir, dst_dir):
-    for z in range(7, 15):
+    if not os.path.exists(dst_dir): os.makedirs(dst_dir)
+    for z in range(3, 14):
         src = src_dir + '/z' + str(z) + '/*.json'
-        command = 'tippecanoe -e %s -l hillshade -z %d -Z %d -P -s EPSG:3857 --no-tile-compression %s' % (dst_dir, z, z, src)
+        dst = dst_dir + '/z/' + str(z)
+    	if not os.path.exists(dst): os.makedirs(dst)
+
+	# -pC no tile compression
+        command = 'tippecanoe -F -e %s -l hillshade -z %d -Z %d -P -s EPSG:3857 %s	' % (dst, z, z, src)
+        #command = 'tippecanoe -F -o %s -l landcover -z %d -Z %d -P -s EPSG:3857 %s' % ('tdata/landcover.mbtiles', z, z, src)
         print command
         subprocess.call(command, shell=True)
 
+	mv = 'mv %s %s' % (dst + '/' + str(z) + '/', dst_dir)
+        print mv
+        subprocess.call(mv, shell=True)
+
 if __name__ == '__main__':
-    vtile('vdata/polygon3', 'vdata/vtile3')
+    vtile('sdata/polygon', 'sdata/vtile')
 
 
