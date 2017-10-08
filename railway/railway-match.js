52afa3f5cc916cffa8e67e83035f9577a79ff6a7
tattii <yuta.tatti@gmail.com>
2017-10-08 17:59:51 +0900

    refacotring

--- railway/railway-match.js
+++ railway/railway-match.js
@@ -1,80 +1,110 @@
 var linematch = require('./linematch');
 var fs = require('fs');
 
-var json = fs.readFileSync("railway/all2/railway.geojson");
+
+// read input geojson
+var json = fs.readFileSync(process.argv[2]);
 var geojson = JSON.parse(json);
 
-var railway = {};
-var railway_prop = {};
-for (i in geojson.features){
-	var feature = geojson.features[i];
-	var name = geojson.features[i].properties.NAME;
 
-	if (!name) continue;
+var railways = filter(geojson);
+var matched = match(railways.lines);
 
-	if (name.match("旅客鉄道")){
-		name = name.replace(/.*旅客鉄道/, "JR");
-	}
+writeGeojson(geojson.crs, matched, railways.props);
 
-	if (name.match("新幹線") || name.match("JR")){
-		//console.log(name);
-		if (!railway[name]){
-			railway[name] = [];
-			railway_prop[name] = feature.properties;
-		}
-		railway[name].push([feature.geometry.coordinates]);
-	}
-}
 
-console.log(railway.length);
 
-var matched = {};
-for (name in railway){
-	matched[name] = [];
-	var lines = railway[name];
+function filter(geojson){
+    var railway = {};
+    var railway_prop = {};
 
-	// sort with length
-	lines.sort(function(a, b){ return b[0].length - a[0].length });
+    for (i in geojson.features){
+        var feature = geojson.features[i];
+        var name = geojson.features[i].properties.NAME;
 
-	for (l in lines){
-		if (l == 0){
-			matched[name] = lines[0];
-			continue;
-		}
-		var line = lines[l];
+        if (!name) continue;
 
-		// linematch
-		// https://github.com/mapbox/linematch
-		//var result = linematch(line, matched, 0.0001);
-		//var result = linematch(line, matched[name], 20);
-		var result = linematch(line, matched[name], 100);
+        if (name.match("旅客鉄道")){
+            name = name.replace(/.*旅客鉄道/, "JR");
+        }
 
-		if (result.length) matched[name].push(result[0]);
-	}
-	console.log(name, matched[name].length);
+        // normalize
+        name = name.split(/ |;|-/)[0];
+
+
+
+        if (name.match("新幹線") || name.match("JR")){
+            //console.log(name);
+            if (!railway[name]){
+                railway[name] = [];
+                railway_prop[name] = feature.properties;
+            }
+            railway[name].push([feature.geometry.coordinates]);
+        }
+    }
+    console.log(Object.keys(railway).length);
+
+    return { lines: railway, props: railway_prop };
 }
 
 
-var data = {
-	type: "FeatureCollection",
-	src: geojson.crs,
-	features: []
-};
+function match(railway){
+    var matched = {};
 
-for (name in matched){
-	var line = matched[name];
-	for (m in line){
-		data.features.push({
-			type: "Feature",
-			geometry: {
-				type: "LineString",
-				coordinates: line[m]
-			},
-			properties: railway_prop[name]
-		});
-	}
+    // for each railway lines
+    for (name in railway){
+        matched[name] = [];
+        var lines = railway[name];
+
+        // sort with length
+        lines.sort(function(a, b){ return b[0].length - a[0].length });
+
+        // for each line segments
+        // pre-merged with names (ST_LineMerge)
+        for (l in lines){
+            var line = lines[l];
+            if (l == 0){
+                matched[name] = line;
+                continue;
+            }
+
+            // linematch
+            // https://github.com/mapbox/linematch
+            // linematch will return a new array 
+            // that contains all segments in a that are not matched with segments in b
+            var result = linematch(line, matched[name], 0.0001);
+            //var result = linematch(line, matched[name], 20);
+            //var result = linematch(line, matched[name], 100);
+
+            if (result.length) matched[name].push(result[0]);
+        }
+
+        console.log(name, matched[name].length);
+    }
+    return matched;
 }
 
-fs.writeFile('railway/railway-match.json', JSON.stringify(data));
+function writeGeojson(crs, matched, props){
+    var data = {
+        type: "FeatureCollection",
+        src: geojson.crs,
+        features: []
+    };
 
+    for (name in matched){
+        var line = matched[name];
+        for (m in line){
+            data.features.push({
+                type: "Feature",
+                geometry: {
+                    type: "LineString",
+                    coordinates: line[m]
+                },
+                properties: props[name]
+            });
+        }
+    }
+
+    fs.writeFile('railway-match.json', JSON.stringify(data));
+}
 
